4c79a286aecc679d68c71b0c54ef7a7ffba357c2
592McAvoy <1245709462@qq.com>
2018-07-04 09:04:39 +0800

    gai

--- code/front-end/paperclip/src/components/nav-bar.js
+++ code/front-end/paperclip/src/components/nav-bar.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import logo from '.././logo.svg';
-import { Input,Icon, Avatar } from 'antd';
+import { Input,Icon, Avatar, Select } from 'antd';
 import { Row, Col } from 'antd';
 import { Anchor } from 'antd';
 import {Link} from 'react-router-dom';
 import {log} from '../pages/loginpage';
 import {log1} from '../pages/RegisterPage';
+
 const Search = Input.Search;
+const Option = Select.Option;
 
 class NavBar extends Component{
     constructor(props){
@@ -25,8 +27,17 @@ class NavBar extends Component{
         }
         this.setState({searchIdx:idx});
     }
+    selectChange(value){
+        console.log("select idx: " + value);
+    }
 
     render() {
+        const selectBefore = (
+            <Select defaultValue="title" style={{ width: 90 }} onChange={this.selectChange}>
+                <Option value="title">标题</Option>
+                <Option value="author">作者</Option>
+            </Select>
+        );
         const buttonAfter = (
             <Link to={"/search/"+this.state.searchIdx}>
                 <Icon type="search" />
@@ -34,6 +45,7 @@ class NavBar extends Component{
         )        
         const search = (
                 <Search
+                addonBefore={selectBefore}
                 placeholder="input search text"
                 enterButton={buttonAfter}
                 onChange={this.changeSearchIdx}
@@ -44,9 +56,10 @@ class NavBar extends Component{
                 <Anchor>
                     <Row type="flex" align="middle" justify="center">
                         <Col span={5}><Link to="/"><img src={logo} width="60px" height="60px" alt="logo"/>Paperclip</Link></Col>
+                        <Col span={1}><Link to='/discover'>首页</Link></Col>
                         <Col span={1}><Link to='/discover'>发现</Link></Col>
                         <Col span={8}>{search}</Col>
-                        <Col span={1} offset={8}>
+                        <Col span={1} offset={7}>
                             <Link to="/user"><Avatar style={{ backgroundColor: '#87d068' }} icon="user" /></Link>
                         </Col>
                         <Col span={1}>user</Col>
@@ -59,9 +72,10 @@ class NavBar extends Component{
                 <Anchor>
                     <Row type="flex" align="middle" justify="center">
                         <Col span={5}><Link to="/"><img src={logo} width="60px" height="60px" alt="logo"/>Paperclip</Link></Col>
+                        <Col span={1}><Link to='/discover'>首页</Link></Col>
                         <Col span={1}><Link to='/discover'>发现</Link></Col>
                         <Col span={8}>{search}</Col>
-                        <Col span={1} offset={8}><Avatar icon="user" /></Col>
+                        <Col span={1} offset={7}><Avatar icon="user" /></Col>
                         <Col span={1}><Link to="/login">Log in</Link></Col>
                     </Row>
                 </Anchor>
--- code/front-end/paperclip/src/pages/homepage.js
+++ code/front-end/paperclip/src/pages/homepage.js
@@ -2,140 +2,23 @@ import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
 import { Input, Select, Carousel, List, Avatar } from 'antd';
 import { Row, Col } from 'antd';
-import { Anchor } from 'antd';
-import logo from '.././logo.svg';
-import {log} from '../pages/loginpage';
-import {log1} from '../pages/RegisterPage';
+import NavBar from '../components/nav-bar';
 
 const Search = Input.Search;
 const Option = Select.Option;
 
 class Home extends Component{
     constructor(props){
-        super(props);
-
-        this.changeSearchIdx = this.changeSearchIdx.bind(this);
-        this.selectChange = this.selectChange.bind(this);
-       
-        this.state = {
-            searchIdx: "empty"
-        }
+        super(props);       
+        
     }
     
-    changeSearchIdx(e){
-        var idx = e.target.value;
-        if(idx == ""){
-            idx = "empty";
-        }
-        this.setState({searchIdx:idx});
-    }
-    selectChange(value){
-        console.log("select idx: " + value);
-    }
-
-    renderNav(){
-        if(log||log1){
-            return(
-                <Anchor>
-                <Row type="flex" align="middle" justify="center">
-                    <Col span={5}><img src={logo} width="60px" height="60px" alt="logo"/>Paperclip</Col>
-                    <Col span={1} offset={17}>
-                        <Link to="user"><Avatar style={{ backgroundColor: '#87d068' }} icon="user" /></Link>
-                    </Col>
-                    <Col span={1}>user</Col>
-                </Row>
-                </Anchor>
-            )
-        }
-        else{
-            return(
-                <Anchor>
-                <Row type="flex" align="middle" justify="center">
-                    <Col span={5}><img src={logo} width="60px" height="60px" alt="logo"/>Paperclip</Col>
-                    <Col span={1} offset={17}><Avatar icon="user" /></Col>
-                    <Col span={1}><Link to="/login">Log in</Link></Col>
-                </Row>
-                </Anchor>
-            )
-        }
-    }
-
-    renderSearch(){
-        const selectBefore = (
-            <Select defaultValue="title" style={{ width: 90 }} onChange={this.selectChange}>
-                <Option value="title">标题</Option>
-                <Option value="author">作者</Option>
-            </Select>
-        );
-        const buttonAfter = (
-            <Link to={"/search/"+this.state.searchIdx}>Search</Link>
-        )        
-        return(
-            <div id="search">
-                <Search
-                addonBefore={selectBefore}
-                placeholder="input search text"
-                enterButton={buttonAfter}
-                size="large"
-                onChange={this.changeSearchIdx}
-                />
-            </div>
-        )
-    }
-
-    renderRecomment(){
-        const data = [
-            {
-              title: 'Ant Design Title 1',
-            },
-            {
-              title: 'Ant Design Title 2',
-            },
-            {
-              title: 'Ant Design Title 3',
-            },
-            {
-              title: 'Ant Design Title 4',
-            },
-          ];
-          
-        return(
-            <Row>
-                <Col span={12}>
-                    <Carousel autoplay>
-                        <div><h3>论文1</h3></div>
-                        <div><h3>论文2</h3></div>
-                        <div><h3>论文3</h3></div>
-                        <div><h3>论文4</h3></div>
-                    </Carousel>
-                </Col>
-                <Col span={12}>
-                    <List
-                        itemLayout="horizontal"
-                        dataSource={data}
-                        renderItem={item => (
-                        <List.Item>
-                            <List.Item.Meta
-                            title={<Link to="/">{item.title}</Link>}
-                            description="Ant Design, a design language for background applications, is refined by Ant UED Team"
-                            />
-                        </List.Item>
-                        )}
-                    />
-                </Col>
-            </Row>
-        )
-    }
     
     render(){
-        const nav = this.renderNav();
-        const search = this.renderSearch();
-        const recomment = this.renderRecomment();
+       
         return(
             <div>
-                {nav}
-                {search}
-                {recomment}
+                <NavBar />
             </div>
         )
     }
