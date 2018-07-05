c724c488c11dcf8bd3a5febf79ba600640a79216
derFischer <905145707@qq.com>
2018-07-05 21:10:24 +0800

    like/star in viewdocpage

--- code/front-end/paperclip/src/pages/viewDocPage.js
+++ code/front-end/paperclip/src/pages/viewDocPage.js
@@ -9,9 +9,12 @@ const constUserName = '用户名';
 const constDocTitle = '文档标题';
 const constDocContent = '一次坐葡萄牙TAP航空从里斯本飞卡萨布兰卡，是那种螺旋桨小飞机，我选到了第一排紧急出口旁边的座位，登机之后正对面就坐着一个空少小哥。在候机厅的时候口渴在自动售货机里吗买了一小听类似芬达的碳酸饮料，后来广播登机了没来得及打开就放到了冲锋衣的兜里登机了。后来飞机升空平稳解开安全带之后我想起来了还有一听饮料准备喝，正好这时候空少小哥在后面，我当时脑子不知道怎么想的鬼使神差的把那听饮料藏在了我那件比较宽大的冲锋衣的袖子里面。小哥回来坐下了之后，我慢慢把袖子竖起来，另一只手伸进去扣住拉环，“呲”的一声拉开了拉环，同时还冒出来一股白烟，之后举起来喝了一口。短短的五秒之内，我观察空少小哥的面部表情至少经历了“疑惑—诧异/恐惧/绝望—劫后余生的喜悦与放松”三种状态。后来一想，要是对面坐的不是空少是空警，我应该已经被当场击毙了吧……';
 const constLikeNo = 10;
-const constCommentNo = 0;
-
+const constCommentNo = 2;
+const constComment = [{user:'大哥', comment:'....'},{user:'大嫂', comment:'.....'}];
 const { TextArea } = Input;
+const constIfLike = false;
+const constIfStar = false;
+
 class ViewDoc extends Component{
     state = {
         uh: '',
@@ -23,7 +26,11 @@ class ViewDoc extends Component{
         docContent: '',
         visible: false,
         mailContent: '',
-
+        commentNo:0,
+        likeNo:0,
+        comment:[],
+        ifLike: false,
+        ifStar: false
     }
     componentWillMount = () => {
         /* get docID from url */
@@ -37,6 +44,11 @@ class ViewDoc extends Component{
             docID: urlDocID,
             docTitle: constDocTitle,
             docContent: constDocContent,
+             comment:constComment,
+             commentNo:constCommentNo,
+             likeNo:constLikeNo,
+             ifLike: constIfLike,
+             ifStar: constIfStar
         })
     }
     showModal = () => {
@@ -64,15 +76,68 @@ class ViewDoc extends Component{
     clearInput = () => {
         this.setState({ mailContent: '' });
     }
+    like = () => {
+        var like = this.state.likeNo;
+        this.setState( {
+            likeNo: like + 1,
+            ifLike: true
+        } )
+    }
+    cancelLike = () => {
+        var like = this.state.likeNo;
+        this.setState( {
+            likeNo: like - 1,
+            ifLike: false
+        })
+    }
+    star = () => {
+        this.setState({
+            ifStar: true
+        })
+    }
+    cancelStar = () => {
+        this.setState({
+            ifStar: false
+        })
+    }
+    renderLikeButton(){
+        if(!this.state.ifLike)
+        {
+            return(
+                <a style={{width:"25%"}} class="navbar-brand" href="#" onClick={this.like}><Icon type="like-o" /> 点赞 ( {this.state.likeNo} ) </a>
+            )
+        }
+        else
+        {
+            return(
+                <a style={{width:"25%"}} class="navbar-brand" href="#" onClick={this.cancelLike}><Icon type="like" /> 取消赞 ( {this.state.likeNo} ) </a>
+            )
+        }
+    }
+
+    renderStarButton(){
+        if(!this.state.ifStar){
+            return(
+                <a style={{width:"25%"}} class="navbar-brand" href="#" onClick={this.star}><Icon type="star-o" /> 收藏本文</a>
+            )
+        }
+        else{
+            return(
+                <a style={{width:"25%"}} class="navbar-brand" href="#" onClick={this.cancelStar}><Icon type="star" /> 取消收藏</a>
+            )
+        }
+    }
     renderBottomNav = () =>{
+        const likeButton = this.renderLikeButton();
+        const starButton = this.renderStarButton();
         return(
             <div className="navbartop">
                 <nav class="navbar navbar-fixed-bottom navbar-default">
                     <div class="container-fluid">
                         <div class="navbar-header" style={{width: "100%", paddingLeft:"25%", paddingRight:"25%", textAlign:"center"}}>
-                            <a style={{width:"25%"}} class="navbar-brand" href="#"><Icon type="like" /> 点赞 </a>
-                            <a style={{width:"25%"}} class="navbar-brand" href="#"><Icon type="star" /> 收藏本文</a>
-                            <a style={{width:"25%"}} class="navbar-brand" href="#"><Icon type="message" /> 评论</a>
+                            {likeButton}
+                            {starButton}
+                            <a style={{width:"25%"}} class="navbar-brand" href="#"><Icon type="message" /> 评论 ( {this.state.commentNo} )</a>
                             <a style={{width:"25%"}} class="navbar-brand" href="#"><Icon type="fork" /> 分享</a>
                         </div>
                     </div>
