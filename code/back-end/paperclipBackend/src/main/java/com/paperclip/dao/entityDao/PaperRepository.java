46120ea2fde93072e8c3581760acb3abf8d0b44d
592McAvoy <1245709462@qq.com>
2018-07-25 09:34:21 +0800

    news

--- code/back-end/paperclipBackend/src/main/java/com/paperclip/dao/entityDao/PaperRepository.java
+++ code/back-end/paperclipBackend/src/main/java/com/paperclip/dao/entityDao/PaperRepository.java
@@ -1,6 +1,7 @@
 package com.paperclip.dao.entityDao;
 
 import com.paperclip.model.Entity.Paper;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,5 +10,6 @@ import java.util.List;
 
 @Repository("paperRepo")
 public interface PaperRepository extends CrudRepository<Paper,Long>{
-
+    @Query("select o from Paper o order by o.star desc")
+    List<Paper> getRecommendPaper();
 }
--- code/back-end/paperclipBackend/src/main/java/com/paperclip/service/SearchService.java
+++ code/back-end/paperclipBackend/src/main/java/com/paperclip/service/SearchService.java
@@ -9,4 +9,14 @@ public interface SearchService {
     // searchpage
     public JSONArray searchPaper(JSONObject data) throws UnsupportedEncodingException;
 
+    //推荐
+    JSONArray getRecommendPaper() throws UnsupportedEncodingException;
+    JSONArray getRecommendNote() throws UnsupportedEncodingException;
+
+    //动态
+    JSONArray getUserStarPaperNews(JSONObject data) throws UnsupportedEncodingException;
+    JSONArray getUserStarNoteNews(JSONObject data) throws UnsupportedEncodingException;
+    JSONArray getUserLikeNoteNews(JSONObject data) throws UnsupportedEncodingException;
+    JSONArray getUserWriteNoteNews(JSONObject data) throws UnsupportedEncodingException;
+
     }
